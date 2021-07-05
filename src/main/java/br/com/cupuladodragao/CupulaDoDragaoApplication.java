9e6b2a7c0322803349e7bce0e0110f42c4f4c9c4
lazhc <lazhcsilva@gmail.com>
2021-07-05 13:40:16 -0300

    Criando repositorio do projeto

---
+++ src/main/java/br/com/cupuladodragao/CupulaDoDragaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.cupuladodragao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupulaDoDragaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupulaDoDragaoApplication.class, args);
+	}
+
+}
